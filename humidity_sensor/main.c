ed8cdaaee3380184484743cb7e1eecb556743cc0
Pradeepa Senanayake <pradeepa.kck@gmail.com>
2020-05-23 14:36:40 +1000

    Added logging to file

--- humidity_sensor/main.c
+++ humidity_sensor/main.c
@@ -2,9 +2,12 @@
 #include <time.h>
 #include "si7021_sensor.h"
 
+static const char temperature_humidity_log_file[] = "/home/debian/si7021_log.log";
+
 int main()
 {
 	SI7021_RET_VAL ret = si7021_init();
+	char log_entry[100];
 
 	if (SI7021_SUCCESS == ret)
 	{
@@ -12,7 +15,14 @@ int main()
 		ret = si7021_read(&reading);
 		if (SI7021_SUCCESS == ret)
 		{
-			printf("%ld, %f, %f\n", time(NULL), reading.temperature, reading.humidity);
+			FILE* fp;
+
+			fp = fopen(temperature_humidity_log_file, "a+");
+			if (fp)
+			{
+				fprintf(fp, "%ld, %f, %f\n", time(NULL), reading.temperature, reading.humidity);
+				fclose(fp);
+			}
 		}
 	}
 
